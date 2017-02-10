df2b9973ca100d25b208078448d55cf2cd74b7d3
Lei2016 <38833433@qq.com>
2017-02-10 14:09:25 +0800

    users can see their orders

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
    resources :products
  end
 
+  namespace :account do
+   resources :orders
+ end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root 'products#index'
   resources :products do
