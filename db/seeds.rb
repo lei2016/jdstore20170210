cf4faa7382182631fa2ebb7d0e7b1017a332adc2
taketimeasafriend <taketimeasafriend@gmail.com>
2017-02-24 16:30:22 +0800

    modify navbar; add search; add seed.rb; modify Index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,13 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 u = User.new
 u.email = "admin@test.com"           # 可以改成自己的 email
-
 u.password = "123456"                # 最少要六码
-
 u.password_confirmation = "123456"   # 最少要六码
-
 u.is_admin = true
 u.save
+puts "建立一个admin账号：admin@test.com,123456. 并添加30个products."
+
+create_products = for i in 1..30 do
+  Product.create!([title: "Product no.#{i}", description: "这是用seed.rb建立的第 #{i} 个默认Product...", quantity: 100, price: rand(50..99)*100])
+end
+puts "30 products created."
